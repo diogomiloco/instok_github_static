ff6530bd04810aa8ff7b4780691b7130e1dad3f4
Diogo <diogo.miloco@gmail.com>
2018-12-26 20:13:02 -0200

    Last commit 26.12.18

--- sw.js
+++ sw.js
@@ -2,24 +2,20 @@ importScripts('/instok_github_static/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/instok_github_static/_nuxt/03c3ed30827fa730acd4.js",
-    "revision": "87eb0d89b63c343733c7a79facd16661"
+    "url": "/instok_github_static/_nuxt/015f4b0ad8bdd709c7d7.js",
+    "revision": "0af32b5dab161424672cefa642bb3804"
   },
   {
-    "url": "/instok_github_static/_nuxt/2ec6e974f0e7c9868e96.js",
-    "revision": "6187cc19feaf4091d5232479512f8ccf"
+    "url": "/instok_github_static/_nuxt/028ae3904ba914609311.js",
+    "revision": "85ebcd4e21ef46d5fd6310a754e34916"
   },
   {
-    "url": "/instok_github_static/_nuxt/362719f0a6685b7c2f21.js",
-    "revision": "8a2791fb66a1057ab2df10cd3806b8c9"
+    "url": "/instok_github_static/_nuxt/145626dcd251908d848e.js",
+    "revision": "3209aa7609a7f8ca13df3861706c10b0"
   },
   {
-    "url": "/instok_github_static/_nuxt/5a1df667c2ff5d140118.js",
-    "revision": "2fb03a3ecba8cb335812ede19f20d4f4"
-  },
-  {
-    "url": "/instok_github_static/_nuxt/647e4ce74c3b6f61c5e9.js",
-    "revision": "61961aba85d00b1001ac777ad7741de6"
+    "url": "/instok_github_static/_nuxt/204f203481c421715ee0.js",
+    "revision": "c981fdb757c4f95270c65788a6500c2d"
   },
   {
     "url": "/instok_github_static/_nuxt/778c6a14bbe2cb9a66e4.js",
@@ -32,6 +28,10 @@ workbox.precaching.precacheAndRoute([
   {
     "url": "/instok_github_static/_nuxt/d651659d7a6456a96171.js",
     "revision": "56d73d7ffc1fb31fd5cce5a9a7b2506e"
+  },
+  {
+    "url": "/instok_github_static/_nuxt/f62d26e5b660bb5d7c30.js",
+    "revision": "43d846bff68c1e7588c7e055f8467e03"
   }
 ], {
   "cacheId": "instok",
