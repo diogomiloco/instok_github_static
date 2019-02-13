136f95da489e5b2142eac2048be5357fe2e21dee
Diogo <diogo.miloco@gmail.com>
2019-02-13 12:17:43 -0200

    Inclusão do GA e melhorias diversas

--- sw.js
+++ sw.js
@@ -2,36 +2,36 @@ importScripts('/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/1d65bd66556991301bcf.js",
-    "revision": "a5ec02b14b8ef377348e3ef2b086651f"
+    "url": "/_nuxt/0d599a6345e6244e2dbd.js",
+    "revision": "febede52c8a1676ec5024f466f9419ad"
   },
   {
-    "url": "/_nuxt/67f84c610fe9fe0532b7.js",
-    "revision": "b1249423565d4277a1636dd8d92d2820"
+    "url": "/_nuxt/73ad5c167785cf440210.js",
+    "revision": "cbce77103900c312be493c082582ed83"
   },
   {
-    "url": "/_nuxt/9f461312e3da725c7dc4.js",
-    "revision": "db0289328ad8e2ad59f968397c799c28"
+    "url": "/_nuxt/87389449b3c8f2fa3361.js",
+    "revision": "39c5dffb812a99a421a1d7d0e0dd4719"
   },
   {
     "url": "/_nuxt/9f9d27d7894fd16d58b4.js",
     "revision": "d91bbf5c5eddcb37d00d608d4bc1416b"
   },
   {
-    "url": "/_nuxt/aa90fed4c11ddec48f5b.js",
-    "revision": "64e7a450c2e1cb2b7276c71b5b9e7ad0"
+    "url": "/_nuxt/b0a725583ce5311448eb.js",
+    "revision": "4d50e6db1ec9f38cf98075b9821c45ce"
   },
   {
-    "url": "/_nuxt/d0ca035e877b7384986d.js",
-    "revision": "16aaa52a15e4a08d06686565dff079d1"
+    "url": "/_nuxt/b7b9b6b0687242e2fefc.js",
+    "revision": "8b4725e4308e964c256c6eda5e605fb5"
   },
   {
     "url": "/_nuxt/d651659d7a6456a96171.js",
     "revision": "56d73d7ffc1fb31fd5cce5a9a7b2506e"
   },
   {
-    "url": "/_nuxt/dd37ee4d6683b5aaa2fe.js",
-    "revision": "e292d7770e7039efb885a454a8a6da48"
+    "url": "/_nuxt/df479fde093688915c00.js",
+    "revision": "cd3d71acc6430bad1a64050b6610d747"
   }
 ], {
   "cacheId": "instok",
@@ -44,4 +44,4 @@ workbox.skipWaiting()
 
 workbox.routing.registerRoute(new RegExp('/_nuxt/.*'), workbox.strategies.cacheFirst({}), 'GET')
 
-workbox.routing.registerRoute(new RegExp('/.*'), workbox.strategies.cacheFirst({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/.*'), workbox.strategies.networkFirst({}), 'GET')
