0aa97fb667eb43c6d8864302e1b698d8784e7297
Diogo <diogo.miloco@gmail.com>
2019-05-25 10:12:17 -0300

    Update Maio/2019

--- sw.js
+++ sw.js
@@ -2,36 +2,36 @@ importScripts('/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/0d599a6345e6244e2dbd.js",
-    "revision": "febede52c8a1676ec5024f466f9419ad"
+    "url": "/_nuxt/6c57de968145da1b3818.js",
+    "revision": "ef57ad28eb6db37deef9222f2a14d853"
   },
   {
-    "url": "/_nuxt/654acb4db706e5be32ec.js",
-    "revision": "18551f7b0f77651487d4499df15c9608"
+    "url": "/_nuxt/7073bcad4b2e868b63d3.js",
+    "revision": "ac4d5eb474bd11220d82bcfcdfb929d9"
   },
   {
-    "url": "/_nuxt/73ad5c167785cf440210.js",
-    "revision": "cbce77103900c312be493c082582ed83"
-  },
-  {
-    "url": "/_nuxt/87389449b3c8f2fa3361.js",
-    "revision": "39c5dffb812a99a421a1d7d0e0dd4719"
+    "url": "/_nuxt/9f9477469885bd552cd3.js",
+    "revision": "045baeec5a06b78e2c0975018999405a"
   },
   {
     "url": "/_nuxt/9f9d27d7894fd16d58b4.js",
     "revision": "d91bbf5c5eddcb37d00d608d4bc1416b"
   },
   {
-    "url": "/_nuxt/b0a725583ce5311448eb.js",
-    "revision": "4d50e6db1ec9f38cf98075b9821c45ce"
+    "url": "/_nuxt/bdf61e9c33da79b0690b.js",
+    "revision": "1b0caa4f3fe8adc2431918c8f513b64f"
   },
   {
-    "url": "/_nuxt/b7b9b6b0687242e2fefc.js",
-    "revision": "8b4725e4308e964c256c6eda5e605fb5"
+    "url": "/_nuxt/d4cf50c63ff645a8ac54.js",
+    "revision": "7bc9419b58eaf643f9882af7c7790145"
   },
   {
     "url": "/_nuxt/d651659d7a6456a96171.js",
     "revision": "56d73d7ffc1fb31fd5cce5a9a7b2506e"
+  },
+  {
+    "url": "/_nuxt/ec7901b7952bc6e170e6.js",
+    "revision": "b03deb9bcc81091260fe4cd9713cede0"
   }
 ], {
   "cacheId": "instok",
